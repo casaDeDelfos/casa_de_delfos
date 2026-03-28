58024dfeccd80e20703f6bb5bb0f2d5231cf8661
casaDeDelfos <heveralvarezmiranda@gmail.com>
2026-03-27 21:12:40 -0500

    Update componentes.js

--- componentes.js
+++ componentes.js
@@ -31,7 +31,22 @@ class MiHeader extends HTMLElement {
         </header>`;
     }
 }
+document.addEventListener("DOMContentLoaded", function () {
+  const accordions = document.querySelectorAll(".accordion");
 
+  accordions.forEach(acc => {
+    acc.addEventListener("click", function () {
+      this.classList.toggle("active");
+      const panel = this.nextElementSibling;
+
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      }
+    });
+  });
+});
 class MiFooter extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
